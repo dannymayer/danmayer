e23cbb690ca02483b5c92b495cb27b9f47435bb3
dannymayer <69997043+dannymayer@users.noreply.github.com>
2023-07-22 16:57:17 -0500

    Site components and structure, import posts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,21 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "#A0A4A3",
-          secondary: "#344134",
-          accent: "#1fb2a6",
-          neutral: "#44545C",
+          "primary": "#A0A4A3",
+          "secondary": "#344134",
+          "accent": "#1fb2a6",
+          "neutral": "#44545C",
           "base-100": "#283135",
-          info: "#6F6D57",
-          success: "#65AC69",
-          warning: "#E39C31",
-          error: "#F43333",
+          "info": "#6F6D57",
+          "success": "#65AC69",
+          "warning": "#E39C31",
+          "error": "#F43333",
         },
       },
     ],
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("daisyui")
+  ],
 };
